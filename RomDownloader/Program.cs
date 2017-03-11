83734a1f5684554ceabef0b31085ecf01cb012bc
Chandler <chndlrtrnr@gmail.com>
2017-03-11 15:35:52 -0600

    heres the new changes

--- RomDownloader/Program.cs
+++ RomDownloader/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RomDownloader.RomSources;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,9 +15,12 @@ namespace RomDownloader
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            DopeRoms r = new DopeRoms();
+            r.GetSystems();
         }
     }
 }
--- RomDownloader/RomSources/DopeRoms.cs
+++ RomDownloader/RomSources/DopeRoms.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RomDownloader.Models;
+using HtmlAgilityPack;
+using System.Net;
 
 namespace RomDownloader.RomSources
 {
@@ -16,7 +18,27 @@ namespace RomDownloader.RomSources
 
         internal override List<GameConsole> GetSystems()
         {
-            throw new NotImplementedException();
+            WebClient webClient = new WebClient();
+            string page = webClient.DownloadString($"{URL}/roms");
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(page);
+
+            var testing = doc.DocumentNode.SelectNodes("//html[1]/body[1]/div[2]/div[1]/div[10]/table[2]/tr[2]/td[1]/font[1]/table[1]/tr[1]/td[1]/table[1]/");
+                //.Descendants()
+                //.Where(n => n.Id == "listitem");
+            //var testing = doc.GetElementbyId("listitem");
+            //var testing = doc.DocumentNode.SelectNodes("//div");
+            //.Where(c => c.Attributes["id"].Value == "listitem")
+            //.ToList();
+
+            //foreach(var c in testing)
+            //{
+            //    var x = c;
+            //}
+
+            return null;
+                
         }
     }
 }
