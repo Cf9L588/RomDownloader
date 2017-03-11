a21c8a6ff8c665c4b9877a864306bf4176309554
Chandler <chndlrtrnr@gmail.com>
2017-03-11 14:10:59 -0600

    Begining build idea

---
+++ RomDownloader/Models/RomSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomDownloader.Models
+{
+    internal abstract class RomSource
+    {
+        internal readonly string Name;
+        internal readonly string URL;
+
+        
+    }
+}
